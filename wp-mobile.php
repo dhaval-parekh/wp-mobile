6c1137ca50388c78695326e01a94cca4fe032b81
Dhaval Parekh <dmparekh007@gmail.com>
2016-10-02 11:27:34 +0530

    overwrite post/page rest response
    - added wrapper on rest response

--- wp-mobile.php
+++ wp-mobile.php
@@ -15,22 +15,32 @@
  * @package   wp-mobile
  * @version   1.0.0
  * @author    Dhaval Parekh <dmparekh007@gmail.com>
- * @copyright 
+ * @copyright
  * @link      http://wpm.ciphersoul.com/
- * @license   
+ * @license
  */
+if ( ! defined( 'REST_NAMESPACE' ) ) {
+	//define( 'REST_NAMESPACE', 'api' );
+}
 
 /**
  * WP_Mobile class
- * 
+ *
  * @access public
  * @since  1.0.0
  */
 class WP_Mobile {
 
 	/**
+	 * rest namespace
+	 *
+	 * @var	string
+	 */
+	private $namespace = 'wp/v2/';
+
+	/**
 	 * plugin directory path
-	 * 
+	 *
 	 * @access	private
 	 * @var		string
 	 * @since	1.0.0
@@ -39,7 +49,7 @@ class WP_Mobile {
 
 	/**
 	 * plugin directory URI
-	 * 
+	 *
 	 * @access	private
 	 * @var		string
 	 * @since	1.0.0
@@ -48,7 +58,7 @@ class WP_Mobile {
 
 	/**
 	 * vendor directory path
-	 * 
+	 *
 	 * @access	private
 	 * @var		string
 	 * @since	1.0.0
@@ -57,7 +67,7 @@ class WP_Mobile {
 
 	/**
 	 * library directory path
-	 * 
+	 *
 	 * @access	private
 	 * @var		string
 	 * @since	1.0.0
@@ -66,50 +76,56 @@ class WP_Mobile {
 
 	/**
 	 * include directory path
-	 * 
+	 *
 	 * @access	private
-	 * @var		string 
+	 * @var		string
 	 * @since	1.0.0
 	 */
 	private $inc_dir = '';
 
 	/**
 	 * construct method
-	 * 
+	 *
 	 * @access	private
-	 * @return	void 
+	 * @return	bool
 	 * @since	1.0.0
 	 */
 	public function __construct() {
-		$this->init();
-	}
-
-	/**
-	 * return instance of WP_Mobile
-	 * 
-	 * @access		public
-	 * @since		1.0.0
-	 * @staticvar	\WP_Mobile		$instance
-	 * @return		\WP_Mobile|bool	
-	 */
-	public function init() {
 		$this->setup();
 		include_once( $this->inc_dir . 'debug.php' );
 		if ( ! $this->is_required_plugin_installed() ) {
-			//	wordpress
 			include_once( $this->vendor_dir . 'class-tgm-plugin-activation.php' );
 			include_once( $this->inc_dir . 'required_plugins.php' );
 			return false;
 		}
+		if ( defined( 'REST_NAMESPACE' ) ) {
+			$this->namespace = REST_NAMESPACE;
+		}
+		add_action( 'init', array( $this, 'init' ), 15 );
+		return true;
+	}
+
+	/**
+	 * return instance of true
+	 *
+	 * @access		public
+	 * @since		1.0.0
+	 * @staticvar	\WP_Mobile		$instance
+	 * @return		\WP_Mobile|bool
+	 */
+	public function init() {
+		$this->includes();
+		$this->overwrite_rest_api();
+		//display(wp_get_current_user());
 		return true;
 	}
 
 	/**
 	 * setup plugin variable
-	 * 
+	 *
 	 * @access	public
 	 * @since	1.0.0
-	 * @return	void 
+	 * @return	void
 	 */
 	private function setup() {
 		// Main plugin directory path and URI.
@@ -121,10 +137,15 @@ class WP_Mobile {
 		$this->lib_dir = trailingslashit( $this->plugin_path . 'lib' );
 		$this->inc_dir = trailingslashit( $this->plugin_path . 'inc' );
 	}
-	
+
+	private function includes() {
+		//	include inc
+		require_once $this->inc_dir . 'rest-api/class-wp-mobile-posts-controller.php';
+	}
+
 	/**
 	 * check requried plugin installed or not
-	 * 
+	 *
 	 * @access	public
 	 * @since	1.0.0
 	 * @return	boolean
@@ -136,11 +157,64 @@ class WP_Mobile {
 			'butterbean/butterbean.php',
 		);
 		foreach ( $requvired_plugin_list as $plugin ) {
-			if ( !is_plugin_active( $plugin ) ) {
+			if ( ! is_plugin_active( $plugin ) ) {
 				return false;
 			}
 		}
 		return true;
+	}
+
+	private function overwrite_rest_api() {
+		global $wp_post_types;
+		add_filter( 'get_rest_namespace', array( $this, 'get_rest_namespace' ), 15 );
+
+		if ( isset( $wp_post_types['post'] ) ) {
+			$wp_post_types['post']->rest_controller_class = 'WP_Mobile_Posts_Controller';
+		}
+		if ( isset( $wp_post_types['page'] ) ) {
+			$wp_post_types['page']->rest_controller_class = 'WP_Mobile_Posts_Controller';
+		}
+
+		add_filter( 'get_post_items', array( $this, 'overwrite_rest_api_response' ), 15, 1 );
+		add_filter( 'get_post_item', array( $this, 'overwrite_rest_api_response' ), 15, 1 );
+		add_filter( 'create_post_item', array( $this, 'overwrite_rest_api_response' ), 15, 1 );
+		add_filter( 'update_post_item', array( $this, 'overwrite_rest_api_response' ), 15, 1 );
+		add_filter( 'delete_post_item', array( $this, 'overwrite_rest_api_response' ), 15, 1 );
+
+		add_filter( 'get_page_items', array( $this, 'overwrite_rest_api_response' ), 15, 1 );
+		add_filter( 'get_page_item', array( $this, 'overwrite_rest_api_response' ), 15, 1 );
+		add_filter( 'create_page_item', array( $this, 'overwrite_rest_api_response' ), 15, 1 );
+		add_filter( 'update_page_item', array( $this, 'overwrite_rest_api_response' ), 15, 1 );
+		add_filter( 'delete_page_item', array( $this, 'overwrite_rest_api_response' ), 15, 1 );
+
+		do_action( 'wp_mobile_overwrite_rest_api' );
+	}
+
+	public function get_rest_namespace() {
+		return $this->namespace;
+	}
+
+	/**
+	 * add wrapper in the rest api response
+	 *
+	 * @hooked
+	 * @access	public
+	 * @since	1.0.0
+	 * @param	WP_REST_Response $response request object
+	 * @return	WP_REST_Response response object
+	 */
+	public function overwrite_rest_api_response( $response ) {
+		$data = $response->data;
+
+		if ( ! isset( $data['code'] ) ) {
+			$response->data = array(
+				'code'		 => 'rest_success',
+				'message'	 => 'Rest Success',
+				'data'		 => $data,
+			);
+		}
+		unset( $data );
+		return $response;
 	}
 
 }
@@ -149,7 +223,7 @@ if ( ! function_exists( 'wp_mobile_init' ) ) {
 
 	/**
 	 * initilized wp_mobile plugin
-	 * 
+	 *
 	 * @access	public
 	 * @since	1.0.0
 	 * @return	void
@@ -159,4 +233,3 @@ if ( ! function_exists( 'wp_mobile_init' ) ) {
 	}
 	wp_mobile_init();
 }
-
