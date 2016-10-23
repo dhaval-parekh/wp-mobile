4ea8b1c4a2f1b314081e83556e55bebefb0de1ce
Dhaval Parekh <dmparekh007@gmail.com>
2016-10-23 18:36:40 +0530

    overwrite rest response of post type list

---
+++ inc/rest-api/class-wp-mobile-post-types-controller.php
@@ -0,0 +1,35 @@
+<?php
+
+class WP_Mobile_Post_Types_Controller extends WP_REST_Post_Types_Controller {
+
+	public function __construct() {
+		parent::__construct();
+		$this->namespace = apply_filters( 'get_rest_namespace', $this->namespace );
+	}
+
+	/**
+	 * Get a collection of taxonomies.
+	 *
+	 * @since	1.0.0
+	 * @access	public
+	 * @param	WP_REST_Request $request Full details about the request.
+	 * @return	WP_REST_Response|WP_Error
+	 */
+	public function get_items( $request ) {
+		$response = parent::get_items( $request );
+		return apply_filters( 'get_post_type', $response, $request );
+	}
+
+	/**
+	 * Get a single taxonomy.
+	 *
+	 * @since	1.0.0
+	 * @access	public
+	 * @param	WP_REST_Request $request Full details about the request.
+	 * @return	WP_REST_Response|WP_Error
+	 */
+	public function get_item( $request ) {
+		$response = parent::get_item( $request );
+		return apply_filters( 'get_post_types', $response, $request );
+	}
+}
